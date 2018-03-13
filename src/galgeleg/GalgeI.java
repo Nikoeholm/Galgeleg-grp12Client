7911ea4ce7a8b5b7f67c9ca4385b9503b5fdd459
nikoeholm <nikoeholm@nikoeholm>
2018-03-13 12:57:40 +0100

    Client side created

---
+++ src/galgeleg/GalgeI.java
@@ -0,0 +1,24 @@
+package galgeleg;
+
+import game.*;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Khurram Saeed Malik
+ */
+public interface GalgeI extends java.rmi.Remote {
+    //Methods that i want my server to implement and client to call
+    ArrayList getUserWords() throws java.rmi.RemoteException;
+    String getVisibleWords() throws java.rmi.RemoteException;
+    String getWord() throws java.rmi.RemoteException;
+    int getTotalWrongGuess() throws java.rmi.RemoteException;
+    boolean isLastGuessCorrect() throws java.rmi.RemoteException;
+    boolean isGameWon() throws java.rmi.RemoteException;
+    boolean isGameLost() throws java.rmi.RemoteException;
+    boolean isGameOver() throws java.rmi.RemoteException;
+    void guessWord(String guess) throws java.rmi.RemoteException;
+    void resetGame() throws java.rmi.RemoteException;
+    void getWordsFromDR() throws java.rmi.RemoteException;
+   
+}
---
+++ src/game/GameI.java
@@ -0,0 +1,10 @@
+package game;
+
+import galgeleg.GalgeI;
+import java.util.ArrayList;
+
+public interface GameI extends java.rmi.Remote {
+  GalgeI findGame(String name) throws java.rmi.RemoteException;
+  ArrayList<String> getAllPlayers() throws java.rmi.RemoteException;
+  void registerPlayer(String name) throws java.rmi.RemoteException;
+}
